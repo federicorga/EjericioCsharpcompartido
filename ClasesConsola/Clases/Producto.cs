870e990818877ae5e2a9170791ac7f2c54d4c347
FedericoGre <federico.garea@gmail.com>
2024-01-13 16:11:44 -0300

    Se incluye proyecto usuario

---
+++ ClasesConsola/Clases/Producto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionConsola.Clases
+{
+    public class Producto
+    {
+        private int id;
+        private string nameProduct;
+        private string descripcion;
+        private double costo;
+        private double precioVenta;
+        private int stock;
+        private int idUsuario;
+    
+
+
+       public Producto(int id, string nameProduct, string descripcion, double costo, double precioVenta, int stock, int idUsuario)
+        {
+            this.id = id;
+            this.nameProduct = nameProduct;
+            this.descripcion = descripcion;
+            this.costo = costo;
+            this.precioVenta = precioVenta;
+            this.stock = stock;
+            this.idUsuario = idUsuario;
+           
+         
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string NameProduct { get => nameProduct; set => nameProduct = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public double Costo { get => costo; set => costo = value; }
+        public double PrecioVenta { get => precioVenta; set => precioVenta = value; }
+        public int Stock { get => stock; set => stock = value; }
+        public int IdUsuario { get => idUsuario; set => idUsuario = value; }
+      
+    }
+}
---
+++ ClasesConsola/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+using AplicacionConsola.Clases;
+
+Usuario usuario1 = new Usuario(12, "Fede", "Garea", "Altair", 1234,"federico.garea@gmail.com");
+
+Producto producto1 = new Producto(1, "Pera", "Es una pera", 21.15, 23.22, 5, usuario1.Id);
+
+Venta venta1 = new Venta(14, "Se vendio una Pera", usuario1.Id);
+
+ProductoVendido productoVendido1 = new ProductoVendido(1, producto1.Id, 2, venta1.Id);
+
+Console.WriteLine("USUARIO:");
+MostrarObjeto(usuario1);
+Console.WriteLine("\n");
+Console.WriteLine("PRODUCTO:");
+MostrarObjeto(producto1);
+Console.WriteLine("\n");
+Console.WriteLine("VENTA:");
+MostrarObjeto(venta1);
+Console.WriteLine("\n");
+Console.WriteLine("PRODUCTO VENDIDO:");
+MostrarObjeto(productoVendido1);
+
+
+void MostrarObjeto(object objeto)
+{
+    Type tipoObjeto = objeto.GetType();
+    foreach (var propiedad in tipoObjeto.GetProperties())
+    {
+        Console.WriteLine($"{propiedad.Name}: {propiedad.GetValue(objeto)}");
+    }
+}
