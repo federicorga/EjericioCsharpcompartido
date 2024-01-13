c8fbc81280540ba03eb5ea59d04b55e9c7c1fa03
unknown <maxi.lizarraga.91@gmail.com>
2024-01-13 15:43:09 -0300

    se agregan jugadores

--- LosMejoresDeLaClase/Program.cs
+++ LosMejoresDeLaClase/Program.cs
@@ -4,6 +4,9 @@ using LosMejoresDeLaClase.Clases;
 
 Jugador Messi = new Delantero("Medio","Lionel Messi",45,87,90,94,99,99);
 Jugador Ronaldo = new Delantero("Medio", "Ronaldo", 45, 87, 90, 94, 99, 99);
+Jugador Mbappe = new Delantero("Derecha", "Mbappe", 45, 87, 90, 94, 99, 99);
 
 PrintConsola.PrintDatos(Messi);
+PrintConsola.PrintDatos(Ronaldo);
+PrintConsola.PrintDatos(Mbappe);
 
